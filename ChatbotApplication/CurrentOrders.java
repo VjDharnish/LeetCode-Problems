f58a329075f6c391ca04819958b61a2f3b11e13f
DHARANISH S <68860373+VjDharnish@users.noreply.github.com>
2023-01-22 22:10:46 +0530

    Add files via upload

---
+++ ChatbotApplication/CurrentOrders.java
@@ -0,0 +1,17 @@
+package ChatService;
+
+import java.util.Scanner;
+
+public class CurrentOrders {
+	CurrentOrders(){
+		System.out.println("you have ordered TV will be dispatched soon");
+		System.out.println("9->Back");
+		System.out.println("0->Exit");
+		Scanner sc = new Scanner(System.in);
+		int choice = sc.nectInt();
+		sc.close();
+		switch(c)
+	}
+	
+
+}
