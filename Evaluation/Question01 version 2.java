c0b20720f0d4b32d0f06aff78eeaab93348d6b8c
DHARANISH S <68860373+VjDharnish@users.noreply.github.com>
2023-02-03 10:13:36 +0530

    Create Question01 version 2.java

---
+++ Evaluation/Question01 version 2.java
@@ -0,0 +1,64 @@
+import java.util.Stack;
+
+public class Question1 {
+	private static boolean isOperator(char ch) {
+		switch(ch) {
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			return true;
+		default:
+			return false;
+		}
+	}
+	private static boolean isVariable(char ch) {
+		return (ch>= 'a' && ch<='z'|| ch>='A' &&ch >='Z');
+	}
+	
+	static boolean validExp(String input) {
+		if(isOperator(input.charAt(0)) || isOperator(input.charAt(input.length()-1))){
+				return false;
+		}
+	//	if(input.charAt(0)==')' || input.charAt(input.length()-1)=='(')
+	//		return false;
+		Stack <Character> stack = new Stack<>();
+		int i=0;
+		while(i<input.length()) {
+			char c = input.charAt(i);
+			if(c=='(') {
+				if(input.charAt(i+1)==')')
+					return false;
+				stack.add(c);
+			}
+			else if(c==')') {
+				if(stack.isEmpty()|| isOperator(input.charAt(i-1)))
+					return false;
+				stack.pop();
+			}
+			else if(isOperator(c)) {
+				if(isOperator(input.charAt(i-1))|| input.charAt(i-1) == '(')
+					return false;			
+			}
+			else if(isVariable(c)) {
+				if(i>0 && isVariable(input.charAt(i-1)))
+					return false;
+			}
+			i++;
+			
+		}
+		if(stack.isEmpty()) return true;
+		return false;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input= "a+b()";
+		boolean valid = validExp(input);
+		System.out.println(valid?"valid":"Invalid");
+
+	}
+
+}
